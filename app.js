3f716e883bdf2f66d14a6d348c770d2c0eeb3590
Chimi504 <lhamchimi312@gmail.com>
2025-05-28 21:36:22 +0600

    UserAuth

---
+++ app.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+require('dotenv').config();
+const { createUserTable } = require('./models/userModel');
+
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+// Middlewares
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret: process.env.SESSION_SECRET || 'secretkey',
+resave: false,
+  saveUninitialized: true,
+}));
+
+
+// View engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+
+// Routes
+const adminRoutes = require('./Routes/adminRoutes');
+const authRoutes = require('./Routes/authRoutes');
+const userRoutes = require('./Routes/userRoutes');
+app.use('/', authRoutes);
+app.use('/admin', adminRoutes);
+app.use('/user', userRoutes);
+
+
+
+// Schema creation
+createUserTable(); // Call this after setting up middlewares 
+const { createFoodTable } = require('./models/foodModel');
+createFoodTable();
+
+
+// Server
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
